9f579f94be1ff814c62daf0088c65d387bbc4e45
mlienad <nititrond@gmail.com>
2026-04-04 22:26:28 +0100

    basic io

--- srsd-project/Program.cs
+++ srsd-project/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter commad 1 (string): ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter commad 2 (int):");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Command 1 " + name + "!");
+        Console.WriteLine("Command 2 " + age + "!");
+    }
+}
