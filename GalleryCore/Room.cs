2d6ad1c938da59a577513cd2256d259a61109fe2
mlienad <nititrond@gmail.com>
2026-04-05 16:38:06 +0100

    project structure

---
+++ GalleryCore/Room.cs
@@ -0,0 +1,6 @@
+namespace GalleryCore;
+
+public class Room
+{
+    
+}
---
+++ logread/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace logread;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
