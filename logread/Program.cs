743b1fcd93237f9533f4dcbc7bd2714eecc57e5c
mlienad <nititrond@gmail.com>
2026-04-05 16:54:09 +0100

    small demo

--- logread/Program.cs
+++ logread/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace logread;
+﻿using GalleryCore;
+
+namespace logread;
 
 class Program
 {
     static void Main(string[] args)
     {
+        GalleryState galleryState = new GalleryState();
+        galleryState.CheckState();
         Console.WriteLine("Hello, World!");
     }
 }
